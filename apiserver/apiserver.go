35235322a3cb1ac60206ae6b0817f68702ee7c8e
Walt K <Walter.Kimbrough@Gmail.com>
2016-01-15 23:08:27 -0800

    added endpoints

--- apiserver/apiserver.go
+++ apiserver/apiserver.go
@@ -7,12 +7,16 @@ import (
 
 	"github.com/dgarlitt/practice-go/pkg/shapes"
 	"github.com/gorilla/mux"
+	"./pkg"
 )
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", IndexHandler)
 	router.HandleFunc("/v1/rectangle", RectangleHandler)
+	router.HandleFunc("/v1/square", SquareHandler)
+	router.HandleFunc("/v1/circle", CircleHandler)
+
 	http.Handle("/", router)
 
 	log.Fatal(http.ListenAndServe(":8008", nil))
@@ -27,6 +31,20 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 // RectangleHandler - handles rectangle requests
 func RectangleHandler(w http.ResponseWriter, r *http.Request) {
 	rec := shapes.Rectangle{Length: 3, Width: 4, Name: "generatedRectangle"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rec)
+}
+
+//SquareHandler - handles square requests
+func SquareHandler(w http.ResponseWriter, r *http.Request) {
+	rec := pkg.Square{Length: 4, Width: 6, Name: "generatedSquare"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rec)
+}
+
+//CircleHandler - handles square requests
+func CircleHandler(w http.ResponseWriter, r *http.Request) {
+	rec := pkg.Square{Length: 3, Width: 2, Name: "generatedCircle"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(rec)
 }
